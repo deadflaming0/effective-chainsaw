f8f24d60e612946820af5d8ddc88190e1f4e5364
deadflaming0 <deadflaming0@proton.me>
2025-04-01 19:01:24 -0300

    Add more structure to hash/pseudorandom functions
    Still not good: maybe there's a clever way to deal with multimethods?

--- src/effective_chainsaw/core.clj
+++ src/effective_chainsaw/core.clj
@@ -1,69 +1,135 @@
 (ns effective-chainsaw.core
-  (:import
-    (org.bouncycastle.crypto.digests SHA256Digest SHA512Digest SHAKEDigest)
-    (org.bouncycastle.crypto.macs HMac)
-    (org.bouncycastle.crypto.generators MGF1BytesGenerator)))
+  (:import (org.bouncycastle.crypto.digests SHA256Digest SHA512Digest SHAKEDigest)
+           (org.bouncycastle.crypto.generators MGF1BytesGenerator)
+           (org.bouncycastle.crypto.macs HMac)
+           (org.bouncycastle.util.encoders Hex)))
 
-;; in order to instantiate for shake and sha2 we'll need:
+;; shake specific:
+(defn shake256-algorithm [] (SHAKEDigest. 256))
 
-;; 1) shake:
-;; * shake256
+;; sha2, security category 1:
+(defn sha-256-algorithm [] (SHA256Digest.))
+(defn mgf1-sha-256-algorithm [] (MGF1BytesGenerator. (sha-256-algorithm)))
+(defn hmac-sha-256-algorithm [] (HMac. (sha-256-algorithm)))
 
-(SHAKEDigest. 256)
+;; sha2, security category 3 and 5:
+(defn sha-512-algorithm [] (SHA512Digest.))
+(defn mgf1-sha-512-algorithm [] (MGF1BytesGenerator. (sha-512-algorithm)))
+(defn hmac-sha-512-algorithm [] (HMac. (sha-512-algorithm)))
 
-;; 2) sha2:
-;; security category 1:
-;; * mgf1-sha-256
-;; * sha-256
-;; * hmac-sha-256
-;; security category 3 and 5:
-;; * mgf1-sha-512
-;; * sha-512
-;; * sha-256
-;; * hmac-sha-512
-
-;; 1:
-(MGF1BytesGenerator. (SHA256Digest.))
-(SHA256Digest.)
-(HMac. (SHA256Digest.))
-
-;; 3 and 5:
-(MGF1BytesGenerator. (SHA512Digest.))
-(SHA512Digest.)
-(HMac. (SHA512Digest.))
-
-;; building blocks:
-
-;; * H_msg
-;; * PRF
-;; * PRF_msg
-;; * F
-;; * H
-;; * Tl
-
-(def parameter-set->parameters+functions
-  {:slh-dsa-sha2-128s {:parameters
-                       {:n 16
+(def parameter-set->parameters ;; add sha2 variants later
+  {:slh-dsa-shake-128s {:n 16
                         :h 63
                         :d 7
-                        :h' 9 ;; find out a consistent way to name keys (when super/sub)
+                        :h' 9
                         :a 12
                         :k 14
                         :lg_w 4
-                        :m 30
-                        :security-category 1}
-                       :functions
-                       {:H_msg (fn [input]
-                                 input)}}})
+                        :m 30}
+   :slh-dsa-shake-128f {:n 16
+                        :h 66
+                        :d 22
+                        :h' 3
+                        :a 6
+                        :k 33
+                        :lg_w 4
+                        :m 34}
+   :slh-dsa-shake-192s {:n 24
+                        :h 63
+                        :d 7
+                        :h' 9
+                        :a 14
+                        :k 17
+                        :lg_w 4
+                        :m 39}
+   :slh-dsa-shake-192f {:n 24
+                        :h 66
+                        :d 22
+                        :h' 3
+                        :a 8
+                        :k 33 :lg_w 4
+                        :m 42}
+   :slh-dsa-shake-256s {:n 32
+                        :h 64
+                        :d 8
+                        :h' 8
+                        :a 14
+                        :k 22
+                        :lg_w 4
+                        :m 47}
+   :slh-dsa-shake-256f {:n 32
+                        :h 68
+                        :d 17
+                        :h' 4
+                        :a 9
+                        :k 35
+                        :lg_w 4
+                        :m 49}})
 
-(defn- parameter-sets
-  []
-  (set (keys parameter-set->parameters+functions)))
+(defn shake256
+  [input output-size]
+  (let [shake256 (shake256-algorithm)
+        input-size (count input)
+        output (byte-array output-size)]
+    (.update shake256 input 0 input-size)
+    (.doFinal shake256 output 0 output-size) ;; check mismatch between output-size and .doFinal
+    (Hex/toHexString output)))
 
-(parameter-sets)
+(defn konkat ;; remove this later, does not make any sense
+  [& inputs]
+  (byte-array (apply concat (map seq inputs))))
 
-(defn- setup
-  [parameter-set]
-  (get parameter-set->parameters+functions parameter-set))
+(defn factories
+  [parameter-set-name]
+  (let [parameters (get parameter-set->parameters parameter-set-name)
+        functions (case parameter-set-name
+                    (:slh-dsa-shake-128s
+                      :slh-dsa-shake-128f
+                      :slh-dsa-shake-192s
+                      :slh-dsa-shake-192f
+                      :slh-dsa-shake-256s
+                      :slh-dsa-shake-256f) ;; shake specific
+                    {:H_msg (fn [R pk-seed pk-root M]
+                              (shake256 (konkat R pk-seed pk-root M)
+                                        (* 8 (:m parameters))))
+                     :PRF (fn [pk-seed sk-seed adrs]
+                            (shake256 (konkat pk-seed adrs sk-seed)
+                                      (* 8 (:n parameters))))
+                     :PRF_msg (fn [sk-prf opt_rand M]
+                                (shake256 (konkat sk-prf opt_rand M)
+                                          (* 8 (:n parameters))))
+                     :F (fn [pk-seed adrs M_1]
+                          (shake256 (konkat pk-seed adrs M_1)
+                                    (* 8 (:n parameters))))
+                     :H (fn [pk-seed adrs M_2]
+                          (shake256 (konkat pk-seed adrs M_2)
+                                    (* 8 (:n parameters))))
+                     :T_l (fn [pk-seed adrs M_l]
+                            (shake256 (konkat pk-seed adrs M_l)
+                                      (* 8 (:n parameters))))}
 
-(setup :slh-dsa-sha2-128s) ;; returns parameters + instantiations of H_msg/PRF/PRF_msg/F/H/Tl?
+                    (:slh-dsa-sha2-128s
+                      :slh-dsa-sha2-128f) ;; security category 1, requires ADRS implementation (and its compression)
+                    (throw (UnsupportedOperationException. "Nice game, pretty boy; but no."))
+
+                    (:slh-dsa-sha2-192s
+                      :slh-dsa-sha2-192f
+                      :slh-dsa-sha2-256s
+                      :slh-dsa-sha2-256f) ;; security category 3 and 5, requires ADRS implementation (and its compression)
+                    (throw (UnsupportedOperationException. "Nice game, pretty boy; but no.")))]
+    {parameter-set-name
+     {:parameters parameters
+      :functions functions}}))
+
+(def H_msg
+  (-> :slh-dsa-shake-128s
+      factories
+      :slh-dsa-shake-128s
+      :functions
+      :H_msg))
+
+(H_msg (byte-array 0x01) ;; R
+       (byte-array 0x01) ;; pk-seed
+       (byte-array 0x01) ;; pk-root
+       (byte-array 0x01) ;; M
+       )
