c77cb75c7245080f3dd69754f76befe86f0b59aa
deadflaming0 <deadflaming0@proton.me>
2025-05-09 11:12:16 -0300

    Minor changes in `address` and `common`

--- src/effective_chainsaw/internals/address.clj
+++ src/effective_chainsaw/internals/address.clj
@@ -3,23 +3,18 @@
 
 (def ^:private address-size 32) ;; understand how to deal with compressed addresses
 
-(def ^:private validate-length!
-  (partial common/validate-length! address-size))
-
 (defn set-layer-address
   [adrs l]
-  (validate-length!
-   (common/merge-bytes
-    (common/integer->byte-array l 4)
-    (common/slice-bytes adrs 4 32))))
+  (common/merge-bytes
+   (common/integer->byte-array l 4)
+   (common/slice-bytes adrs 4 32)))
 
 (defn set-tree-address
   [adrs t]
-  (validate-length!
-   (common/merge-bytes
-    (common/slice-bytes adrs 0 4)
-    (common/integer->byte-array t 12)
-    (common/slice-bytes adrs 16 32))))
+  (common/merge-bytes
+   (common/slice-bytes adrs 0 4)
+   (common/integer->byte-array t 12)
+   (common/slice-bytes adrs 16 32)))
 
 (def ^:private addresses-types
   {:wots-hash 0
@@ -32,36 +27,32 @@
 
 (defn set-type-and-clear
   [adrs Y] ;; Y is a keyword converted to integer internally
-  (validate-length!
-   (common/merge-bytes
-    (common/slice-bytes adrs 0 16)
-    (common/integer->byte-array (get addresses-types Y) 4)
-    (common/integer->byte-array 0 12))))
+  (common/merge-bytes
+   (common/slice-bytes adrs 0 16)
+   (common/integer->byte-array (get addresses-types Y) 4)
+   (common/integer->byte-array 0 12)))
 
 (defn set-key-pair-address
   [adrs i]
-  (validate-length!
-   (common/merge-bytes
-    (common/slice-bytes adrs 0 20)
-    (common/integer->byte-array i 4)
-    (common/slice-bytes adrs 24 32))))
+  (common/merge-bytes
+   (common/slice-bytes adrs 0 20)
+   (common/integer->byte-array i 4)
+   (common/slice-bytes adrs 24 32)))
 
 (defn set-chain-address
   [adrs i]
-  (validate-length!
-   (common/merge-bytes
-    (common/slice-bytes adrs 0 24)
-    (common/integer->byte-array i 4)
-    (common/slice-bytes adrs 28 32))))
+  (common/merge-bytes
+   (common/slice-bytes adrs 0 24)
+   (common/integer->byte-array i 4)
+   (common/slice-bytes adrs 28 32)))
 
 (def set-tree-height set-chain-address)
 
 (defn set-hash-address
   [adrs i]
-  (validate-length!
-   (common/merge-bytes
-    (common/slice-bytes adrs 0 28)
-    (common/integer->byte-array i 4))))
+  (common/merge-bytes
+   (common/slice-bytes adrs 0 28)
+   (common/integer->byte-array i 4)))
 
 (def set-tree-index set-hash-address)
 
